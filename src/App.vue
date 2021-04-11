5f19d4823213f5149255374d223966df2f05f1f9
Matthieu MARTIN <codingstuff91@gmail.com>
2021-04-11 16:57:09 +0200

    Add Buefy framework, edit register view and add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/products">Produits</router-link> |
-      <router-link to="/register">Inscription</router-link>
-    </div>
-    <router-view/>
+      <b-navbar>
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="./assets/logo.png"
+                >
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="/">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a href="/register" class="button is-primary">
+                        <strong>Inscription</strong>
+                    </a>
+                    <a class="button is-light">
+                        Connexion
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+      </b-navbar>
+
+      <div class="container mt-4">
+        <router-view/>
+      </div>
+
   </div>
 </template>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,40 @@
 <template>
-    <div>
-        <form @submit.prevent="register">
-            <label>Username</label>
-            <input type="text" v-model="user.username">
+    <div class="container">
+        <b-notification type="is-primary" aria-close-label="Close notification">
+            Inscrivez vous sur cette plateforme, pour apprendre a utiliser Laravel SANCTUM
+        </b-notification>
 
-            <label>Email</label>
-            <input type="text" v-model="user.email">
+        <section>
+            <div class="columns">
+                <div class="column is-half">
+                    <img alt="logo" src="../assets/laravel-sanctum.png">
+                </div>
+                <div class="column"> 
+                    <div class="column is-full">
+                        <b-field label="Username" label-position="on-border">
+                            <b-input value="johnsilver" v-model="user.username"></b-input>
+                        </b-field>
+                    </div>
 
-            <label>Password</label>
-            <input type="text" v-model="user.password">
+                    <div class="column is-full">
+                        <b-field label="Email" label-position="on-border">
+                            <b-input type="email" v-model="user.email"></b-input>
+                        </b-field>
+                    </div>
 
-            <input type="submit" value="inscription">
-        </form>
+                    <div class="column is-full">
+                        <b-field label="Password" label-position="on-border">
+                            <b-input type="password" 
+                                v-model="user.password">
+                            </b-input>
+                        </b-field>
+                    </div>
+                </div>
+            </div>
+
+            <b-button type="is-success" @click="register" class="mt-6">Inscription</b-button>
+
+        </section>
     </div>
 </template>
 
@@ -30,10 +53,26 @@ const BASE_URL = "http://localhost:8000/api"
             register() {
                 axios.post(BASE_URL + '/user', {
                     user : this.user
-                }).then((response)=>{
-                    localStorage.setItem('test-token', response.data.Token)
+                }).then(()=>{
+                    this.$buefy.dialog.confirm({
+                        title: 'Succès',
+                        message: 'Nouveau compte utilisateur crée !!',
+                        confirmText: 'Fermer',
+                        type: 'is-success',
+                        hasIcon: true                    
+                    })
                 }).catch((error)=> {
-                    console.log(error.response.data.errors)
+                    console.log(error);
+                    this.$buefy.dialog.alert({
+                        title: 'Error',
+                        message: error.message,
+                        type: 'is-danger',
+                        hasIcon: true,
+                        icon: 'times-circle',
+                        iconPack: 'fa',
+                        ariaRole: 'alertdialog',
+                        ariaModal: true
+                    })                
                 })
             }
         },
